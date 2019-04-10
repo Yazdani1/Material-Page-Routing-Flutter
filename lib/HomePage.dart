2f77727f7b67f2bfe41c43e53a96d9eeaf75793c
Yazdani <yaz4dani@gmail.com>
2019-04-10 21:13:14 +0600

    Second commit

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -10,7 +10,7 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-
+      
       appBar: new AppBar(
         title: new Text("Flutter Routing"),
         backgroundColor: Colors.redAccent,
